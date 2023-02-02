de27bb8d63df5140330afb41dfa4f8e80741eec9
akinj15 <akin.bezerra123@gmail.com>
2023-02-02 23:13:11 +0000

    Update HomeComponent.vue and TableComponent.vue

--- src/components/home/HomeComponent.vue
+++ src/components/home/HomeComponent.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    <TableComponent 
-      :names="names"
-      :itens="array"
-    />
+  <div class="home-container">
+    <TableComponent :names="names" :itens="array" />
   </div>
 </template>
 <script lang="ts">
-import HomeComponent from './HomeController'
-export default HomeComponent
+import HomeComponent from "./HomeController";
+export default HomeComponent;
 </script>
+<style scoped>
+.home-container {
+  padding: 20px;
+  overflow: hidden;
+}
+</style>
--- src/components/styled-compenets/table/TableComponent.vue
+++ src/components/styled-compenets/table/TableComponent.vue
@@ -1,35 +1,52 @@
 <template>
+  <div class="corner">
     <table>
       <thead>
         <tr>
+          <th>select</th>
           <th v-for="name in props.names">
             {{ name }}
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in itens">
+        <tr v-for="item in itens" @click="seleciona()" :style="style">
           <td><input type="checkbox" /></td>
-          <td v-for="key in Object.keys(item)">{{item[key]}}</td>
+          <td v-for="key in Object.keys(item)">{{ item[key] }}</td>
         </tr>
       </tbody>
     </table>
-
+  </div>
 </template>
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted } from "vue";
 const props = defineProps({
   names: Array,
   itens: Array,
-})
+});
 
-let tableItensKeys = ref(null)
-
+let tableItensKeys = ref(null);
+let selected = ref(false);
+let style = ref({});
+let seleciona = () => {
+  selected = !selecterd;
+  style = {
+    background: selected ? "rgba(240, 216, 254, 1)" : "",
+  };
+};
 onMounted(() => {
-  tableItensKeys.value = Object.keys(props.itens)
-})
-
+  tableItensKeys.value = Object.keys(props.itens);
+});
 </script>
 <style scoped>
-
+thead th,
+tbody td {
+  border: 1px solid rgb(208, 215, 222);
+  padding: 16px;
+}
+table {
+  border-radius: 10px;
+  border-spacing: 0;
+  border-collapse: separate;
+}
 </style>
